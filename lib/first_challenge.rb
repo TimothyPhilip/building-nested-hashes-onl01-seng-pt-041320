bf4872279dd704c5cfb404624dd2b3d6390cecd6
Philippe Timothe <philippetimothe@gmail.com>
2020-04-21 23:21:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,7 +5,7 @@ def first_challenge
     :matriarch => {:name => "Lady Montague", :age => "54"},
     :hero => {:name => "Romeo", :age => "15", :status => "alive"},
     :hero_friends => [
-      :name
+      :name => 
       :age
       :attitude
       ]
