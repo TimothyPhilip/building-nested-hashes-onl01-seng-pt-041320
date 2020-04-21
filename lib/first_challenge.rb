f4690da684869110281672c6b78922231b7211d0
Philippe Timothe <philippetimothe@gmail.com>
2020-04-21 23:29:49 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,10 +5,12 @@ def first_challenge
     :matriarch => {:name => "Lady Montague", :age => "54"},
     :hero => {:name => "Romeo", :age => "15", :status => "alive"},
     :hero_friends => [
-      
-      :name => 
-      :age
-      :attitude
+      { :name => "Benvolio", 
+        :age => "17", 
+        :attitude => "worried"}
+      { :name => "Mercutio", 
+        :age => "18", 
+        :attitude => "hot-headed"}  
       ]
     
   },
@@ -16,7 +18,14 @@ def first_challenge
     :patriarch => {:name => "Lord Capulet", :age => "50"},
     :matriarch => {:name => "Lady Capulet", :age => "51"},
     :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
-    :heroine_friends =>[]
+    :heroine_friends =>[
+      { :name => "Steven", 
+        :age => "30", 
+        :attitude => "confused"}
+      { :name => "Nurse", 
+        :age => "44", 
+        :attitude => "worried"}
+      ]
     
   }
 }
